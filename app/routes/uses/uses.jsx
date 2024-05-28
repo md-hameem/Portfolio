277bf9d204f0e2ce64cbde5ca924875530ed20f8
Mohammad Hamim <hamimmd555@gmail.com>
2024-05-28 20:42:25 +0800

    modified uses

--- app/routes/uses/uses.jsx
+++ app/routes/uses/uses.jsx
@@ -34,34 +34,82 @@ export const Uses = () => {
           opacity={0.7}
         />
         <ProjectHeader
-          title="Uses"
-          description="A somewhat comprehensive list of tools, apps, hardware, and more that I use on a daily basis to design and code things. And yeah, that is a Johnny Mnemonic GIF in the background."
+          title="About me"
+          description="Hello! I'm Mohammad Hamim, a dedicated Software Engineering student at Zhengzhou University with a profound interest in Machine Learning and Deep Learning. My passion for technology is reflected in my hands-on experience in developing robust web applications and innovative software solutions"
         />
         <ProjectSection padding="none" className={styles.section}>
           <ProjectSectionContent>
             <ProjectTextRow width="m">
-              <ProjectSectionHeading>Design</ProjectSectionHeading>
+              <ProjectSectionHeading>Employment History</ProjectSectionHeading>
               <ProjectSectionText as="div">
+                <p>
+                  <b>
+                    Full Stack Web App Developer (Intern) | Luday AB | Remote | 02/2024 -
+                    04/2024 | Sweden
+                  </b>
+                </p>
+                <p>
+                  During my internship at Luday AB, I gained extensive hands-on experience
+                  with React, Next.js, and Python Flask, developing robust SAAS projects.
+                  My role involved:
+                </p>
+                <br></br>
                 <List>
                   <ListItem>
-                    <Link href="https://www.figma.com">Figma</Link> is my primary tool for
-                    UI design these days. Made the switch from Sketch in 2020 and haven’t
-                    looked back. I’ve also created{' '}
-                    <Link href="https://www.figma.com/@hamish">a few plugins</Link> that
-                    you can install.
+                    Leading the creation of platforms such as a social media management
+                    tool and a family networking app.
                   </ListItem>
                   <ListItem>
-                    Any motion graphics I create are created in Adobe After Effects. So
-                    far I haven’t found a non-Adobe product that’s as good. If anyone has
-                    suggestions please <Link href="/contact">message me</Link>.
+                    Enhancing team productivity through the use of VS Codes Live Share.
                   </ListItem>
                   <ListItem>
-                    For any 3D models and video editing I use{' '}
-                    <Link href="https://www.blender.org/">Blender</Link>. Since 2.8 it’s
-                    become way simpler to use and in a lot of ways better than expensive
-                    paid tools like 3DS Max or Maya.
+                    Achieving a 95% bug-fix success rate by resolving critical issues left
+                    by previous developers.
+                  </ListItem>
+                  <ListItem>
+                    Developing innovative platforms and fostering a collaborative
+                    environment to ensure successful project completion.
                   </ListItem>
                 </List>
+                <p>
+                  Contact: Mr. Dayo - <a href="mailto:dayo@luday.se">dayo@luday.se</a>
+                </p>
+              </ProjectSectionText>
+
+              <ProjectSectionText as="div">
+                <p>
+                  <b>
+                    Full Stack Web Developer | Flamingo Footwear Limited | 01/2021 -
+                    04/2021 | Bangladesh
+                  </b>
+                </p>
+                <p>
+                  At Flamingo Footwear Limited, I was responsible for developing UI
+                  components for global trade platforms and optimizing performance by 78%.
+                  My key responsibilities included:
+                </p>
+                <br></br>
+                <List>
+                  <ListItem>
+                    Creating seamless user experiences and ensuring responsive websites
+                    across various devices.
+                  </ListItem>
+                  <ListItem>
+                    Handling both front-end and back-end tasks with proficiency in
+                    React.js and JavaScript.
+                  </ListItem>
+                  <ListItem>
+                    Designing data models and implementing robust security measures.
+                  </ListItem>
+                  <ListItem>
+                    Integrating payment gateways for e-commerce sites to facilitate smooth
+                    transactions.
+                  </ListItem>
+                </List>
+                <p>
+                  Contact: Md. Jahirul Islam (CEO) -{' '}
+                  <a href="mailto:ceo@ftipvt.com">ceo@ftipvt.com</a>
+                </p>
               </ProjectSectionText>
             </ProjectTextRow>
           </ProjectSectionContent>
@@ -69,45 +117,98 @@ export const Uses = () => {
         <ProjectSection padding="none" className={styles.section}>
           <ProjectSectionContent>
             <ProjectTextRow width="m">
-              <ProjectSectionHeading>Development</ProjectSectionHeading>
+              <ProjectSectionHeading>Skills & technologies</ProjectSectionHeading>
               <ProjectSectionText as="div">
+                <p>
+                  I possess a diverse set of skills and expertise in various technologies,
+                  which have been instrumental in my development as a proficient software
+                  engineer. My technical proficiencies include:
+                </p>
+                <br></br>
+                <p>
+                  <strong>
+                    <em>Programming Languages:</em>
+                  </strong>
+                </p>
                 <List>
                   <ListItem>
-                    I use <Link href="https://vscodium.com/">VSCodium</Link> as my text
-                    editor, with the Tokyo Night theme and Operator Mono as my typeface of
-                    choice.
+                    <b>Python:</b> Extensive experience in developing applications using
+                    Python, including web development with Flask and machine learning
+                    projects with TensorFlow and OpenCV.
                   </ListItem>
                   <ListItem>
-                    Firefox is my main browser for both development and general use.
+                    <b>JavaScript:</b> Proficient in both front-end and back-end
+                    development using JavaScript, with strong command of React and Next.js
+                    for building dynamic user interfaces.
                   </ListItem>
                   <ListItem>
-                    <Link href="https://reactjs.org/">React</Link> is my front end
-                    Javascript library of choice. The component-centric mental model is
-                    the first thing that truly made sense to me as a designer.
+                    <b>Java and C++:</b> Solid foundation in object-oriented programming,
+                    compilers, algorithms, and operating systems.
                   </ListItem>
+                </List>
+                <p>
+                  <strong>
+                    <em>Web Development:</em>
+                  </strong>
+                </p>
+                <List>
                   <ListItem>
-                    For 3D effects and image shaders I use{' '}
-                    <Link href="https://threejs.org/">three.js</Link>. It has a bit of a
-                    learning curve but you can do some really powerful stuff with it.
-                  </ListItem>
-                  <ListItem>
-                    For CSS I’ve used a myriad pre-processors and css-in-js solutions like
-                    styled-components, but these days I’m using vanilla CSS with{' '}
-                    <Link href="https://postcss.org/">PostCSS</Link> to get upcoming CSS
-                    features today.
-                  </ListItem>
-                  <ListItem>
-                    For Javascript animations I use{' '}
-                    <Link href="https://www.framer.com/motion/">Framer Motion</Link>, it’s
-                    a great way to add spring animations to React and three.js.
-                  </ListItem>
-                  <ListItem>
-                    For building and testing UI components in isolation I use{' '}
-                    <Link href="https://storybook.js.org/">Storybook</Link>. Check out the{' '}
-                    <Link href="https://storybook.hamishw.com">
-                      storybook for this website
+                    <Link href="https://reactjs.org/">
+                      <b>React</b>
                     </Link>
-                    .
+                    : Expertise in building responsive and intuitive user interfaces.
+                  </ListItem>
+                  <ListItem>
+                    <Link href="https://nextjs.org/">
+                      <b>Next.js</b>
+                    </Link>
+                    : Experience in developing server-rendered React applications.
+                  </ListItem>
+                  <ListItem>
+                    <Link href="https://flask.palletsprojects.com/">
+                      <b>Flask</b>
+                    </Link>
+                    : Proficient in developing server-side logic and APIs using Flask.
+                  </ListItem>
+                  <ListItem>
+                    <b>Tailwind CSS:</b> Skilled in using Tailwind CSS for creating modern
+                    and responsive designs.
+                  </ListItem>
+                </List>
+                <p>
+                  <strong>
+                    <em>Machine Learning and Deep Learning:</em>
+                  </strong>
+                </p>
+                <List>
+                  <ListItem>
+                    <Link href="https://www.tensorflow.org/">
+                      <b>TensorFlow</b>
+                    </Link>
+                    : Hands-on experience with developing and deploying machine learning
+                    models.
+                  </ListItem>
+                  <ListItem>
+                    <Link href="https://opencv.org/">
+                      <b>OpenCV</b>
+                    </Link>
+                    : Knowledge of computer vision techniques and deep learning frameworks
+                    for image recognition.
+                  </ListItem>
+                </List>
+                <p>
+                  <strong>
+                    <em>Development Tools:</em>
+                  </strong>
+                </p>
+                <List>
+                  <ListItem>
+                    <b>Git</b>: Proficient in using Git for version control and
+                    collaborative development.
+                  </ListItem>
+                  <ListItem>
+                    <b>VS Code:</b> Experienced with Visual Studio Code, including
+                    features like Live Share to enhance team productivity.
                   </ListItem>
                 </List>
               </ProjectSectionText>
@@ -122,35 +223,29 @@ export const Uses = () => {
                 <TableBody>
                   <TableRow>
                     <TableHeadCell>Desktop</TableHeadCell>
-                    <TableCell>Custom built</TableCell>
+                    <TableCell>
+                      Custom built (AMD Ryzen 5 5500X, RX5500XT, 16GB)
+                    </TableCell>
                   </TableRow>
                   <TableRow>
                     <TableHeadCell>Operating system</TableHeadCell>
-                    <TableCell>Arch Linux (by the way)</TableCell>
+                    <TableCell>Windows</TableCell>
                   </TableRow>
                   <TableRow>
                     <TableHeadCell>Monitor</TableHeadCell>
-                    <TableCell>1440p IPS 144hz LG 27GL850</TableCell>
+                    <TableCell>1080p IPS 144hz LG</TableCell>
                   </TableRow>
                   <TableRow>
                     <TableHeadCell>Keyboard</TableHeadCell>
-                    <TableCell>Tofu65</TableCell>
+                    <TableCell>Redragon K552-R KUMARA</TableCell>
                   </TableRow>
                   <TableRow>
                     <TableHeadCell>Mouse</TableHeadCell>
-                    <TableCell>Logitech G403</TableCell>
+                    <TableCell>Razer Viper Mini</TableCell>
                   </TableRow>
                   <TableRow>
                     <TableHeadCell>Laptop</TableHeadCell>
-                    <TableCell>Macbook Pro 14″</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableHeadCell>Headphones</TableHeadCell>
-                    <TableCell>Audio Technica ATH-M50x/Apple Airpods</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableHeadCell>Microphone</TableHeadCell>
-                    <TableCell>Blue Yeti</TableCell>
+                    <TableCell>MSI Modern 15</TableCell>
                   </TableRow>
                 </TableBody>
               </Table>
